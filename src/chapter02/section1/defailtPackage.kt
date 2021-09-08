1991baa43604860aa53f198d2dc3012860bbbe58
Kazto <47233861+Kazto-lsk@users.noreply.github.com>
2021-09-08 10:41:21 +0900

    2교시 - Intro + Num출력
    파이와 정수형 변수 출력하는 프로젝트

---
+++ src/chapter02/section1/defailtPackage.kt
@@ -0,0 +1,12 @@
+package chapter02.section1
+
+fun main() {
+    val intro:String = "Hello";
+    val num:Int = 20;
+
+    println(PI)
+    println(abs(-12.6))
+
+    println("Intro:$intro. Num:  $num")
+
+}
